335077290a8516016f29a95cc0ee278b0350e3b1
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-15 15:16:24 +0300

    start

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import { env } from "./utils/env.js"
+
+export const setupServer = () => {
+
+    const app = express();
+
+    const PORT = Number(env('PORT', '3000'));
+
+    
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+
+
+
+}
